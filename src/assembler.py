8155f9af1b9e60d9b5b8c92a85b24fc4ae318b7a
amirhossein <you@example.com>
2024-01-12 23:18:52 +0330

    three and four section instruction are completed

--- src/assembler.py
+++ src/assembler.py
@@ -1,8 +1,10 @@
 import src.statics
-ADDRESSES = dict()
+
+
 
 
 def assemble(in_file):
+    ADDRESSES = dict()
     with open("./input/" + in_file, "r") as file:
         lines = file.read().split('\n')
         location = 0
@@ -33,10 +35,8 @@ def assemble(in_file):
                         if instruction in src.statics.RI:
                             output[location2] = hex(src.statics.RI[instruction])
                         elif instruction == 'END':
-                            keys = list(output.keys()).sort()
-                            for key in keys:
-                                output[key] = struct.pack('>i', int(output[key], 16))
-                            # right to output file 
+                            None
+                            # write to output file
                         else:
                             print('facing with error')
                             break
@@ -45,9 +45,10 @@ def assemble(in_file):
                             if sections2[1] not in ADDRESSES:
                                 print('variable not defind')
                             else:
-                                output[location2] = hex(int(src.statics.MI[instruction][0] + ADDRESSES[sections2[1]], 16))
+                                output[location2] = hex(
+                                    int(src.statics.MI[instruction][0] + ADDRESSES[sections2[1]], 16))
                         elif instruction == 'ORG':
-                            location2 = int(sections2[1] , 16) - 1
+                            location2 = int(sections2[1], 16) - 1
                             org = location2
                         elif instruction == 'DEC' or instruction == 'HEX':
                             output[location2]
@@ -55,4 +56,16 @@ def assemble(in_file):
                             output[location2] = hex(int(src.statics.RI[sections2[1]]))
                         else:
                             print('eroor')
-            location += 1
+                    elif len(sections2) == 3:
+                        if sections2[2] == 'I':
+                            hex_code = hex(int(src.statics.MI[instruction][1] + ADDRESSES[sections2[1]], 16))
+                        else:
+                            if sections2[1] == 'HEX':
+                                hex_code = hex(int(sections2[2], 16))
+                            else:
+                                hex_code = hex(int(sections2[2]))
+                        output[location2] = hex_code
+                    elif len(sections2) == 4:
+                        hex_code = hex(int(src.statics.MI[sections2[1]] + ADDRESSES[sections2[2]], 16))
+                        output[location2] = hex_code
+                    location2 += 1
